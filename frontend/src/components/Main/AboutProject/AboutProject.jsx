92ff0df2df26d00a2f1b0d1cc3b8bb34daeb23a6
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-06-12 15:27:49 +0300

    сверстать адаптив под 320px для aboutProject

--- frontend/src/components/Main/AboutProject/AboutProject.jsx
+++ frontend/src/components/Main/AboutProject/AboutProject.jsx
@@ -3,18 +3,22 @@ import Style from './AboutProject.module.scss';
 
 export default function AboutProject() {
   return (
-    <div id='about-project' className={Style.conteiner}>
+    <div id="about-project" className={Style.conteiner}>
       <div className={Style.title}>
         <h2>О проекте</h2>
       </div>
       <div className={Style.description}>
-        <h3>Дипломный проект включал 5 этапов</h3>
-        <h3>На выполнение диплома ушло 5 недель</h3>
-        <p>
+        <h3 className={Style.description_title_stages}>
+          Дипломный проект включал 5 этапов
+        </h3>
+        <h3 className={Style.description_title_performance}>
+          На выполнение диплома ушло 5 недель
+        </h3>
+        <p className={Style.description_stages}>
           Составление плана, работу над бэкендом, вёрстку, добавление
           функциональности и финальные доработки.
         </p>
-        <p>
+        <p className={Style.description_performance}>
           У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
           соблюдать, чтобы успешно защититься.
         </p>
