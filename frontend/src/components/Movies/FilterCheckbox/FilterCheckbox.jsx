71db24c7b35d25e5f5190d74142da0f83fdcf6c0
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-06-14 10:23:30 +0300

    добавить второе состояние тогла и убрать подчеркивание сылки

--- frontend/src/components/Movies/FilterCheckbox/FilterCheckbox.jsx
+++ frontend/src/components/Movies/FilterCheckbox/FilterCheckbox.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import Style from './FilterCheckbox.module.scss';
 
 export default function FilterCheckbox() {
+  const [stateTogl, isStateTogl] = React.useState(true);
   return (
     <div className={Style.checkbox_conteiner}>
-      <div className={`${Style.checkbox} ${Style.checkbox_on}`}></div>
+      <div
+        onClick={() => isStateTogl(!stateTogl)}
+        className={`${Style.checkbox} ${
+          stateTogl ? Style.checkbox_on : Style.checkbox_off
+        }`}
+      ></div>
       <p>Короткометражки</p>
     </div>
   );
